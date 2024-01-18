3fc17e0ca1eb51378af30b2c1e2e865043fd030f
aobarbieri <aobarbieri@gmail.com>
2024-01-17 22:00:21 -0500

    Styles for header and main

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,10 @@ import styles from './page.module.css'
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<h1>Create a new shopping list</h1>
-			<Link href='lists/new/'>Create List</Link>
+			<section className={styles.container}>
+				<h1>Create a new shopping list</h1>
+				<Link href='lists/new/'>Create List</Link>
+			</section>
 		</main>
 	)
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,18 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import NavLink from './NavLink'
+import NavLink from './nav-link'
 import logo from '@/assets/logo.svg'
+import styles from './header.module.css'
 
 export default function Header() {
 	return (
-		<header>
+		<header className={styles.header}>
 			<nav>
 				<Link href='/'>
 					<Image src={logo} alt='Food and Stuff logo' width={131} height={31} priority />
 				</Link>
 				<ul>
-					<li>
+					<li className={styles.item}>
 						<NavLink href='/lists'>My Lists</NavLink>
 					</li>
 				</ul>
