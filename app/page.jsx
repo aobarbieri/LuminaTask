7e2ab64c60c1e3a583b58ad239aeec3446bb0780
aobarbieri <aobarbieri@gmail.com>
2024-01-17 22:11:29 -0500

    Rename app/page

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
 		<main className={styles.main}>
 			<section className={styles.container}>
 				<h1>Create a new shopping list</h1>
-				<Link href='lists/new/'>Create List</Link>
+				<Link href='lists/new/'>+ Create List</Link>
 			</section>
 		</main>
 	)
