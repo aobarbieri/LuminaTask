8b5a1b2a9182b36baf2bc5f1a464ed9b46473466
aobarbieri <aobarbieri@gmail.com>
2024-01-18 21:15:55 -0500

    Home styles

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,17 @@
 import Link from 'next/link'
 import styles from './page.module.css'
+import Image from 'next/image'
+import add from '@/assets/icons/add.svg'
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
 			<section className={styles.container}>
 				<h1>Create a new shopping list</h1>
-				<Link href='lists/new/'>+ Create List</Link>
+				<Link href='lists/new/'>
+					<Image src={add} alt='plus' width={17} height={17} />
+					Create list
+				</Link>
 			</section>
 		</main>
 	)
